86b14ac31786ffed1e1406492cd63d7cd80d1876
Priza Toixou <orestis.pps@gmail.com>
2022-07-30 18:25:26 +0300

    Removed boundary node creation from boundary class

--- Mesh/DomainBoundary.cs
+++ Mesh/DomainBoundary.cs
@@ -16,33 +16,10 @@ namespace Mesh
         /// <value></value>
         public double[,] BoundaryNodesCoordinates {get;}
 
-        /// <summary>
-        /// Contains the nodes that form the boundary
-        /// </summary>
-        /// <typeparam name="Node">Nodes</typeparam>
-        /// <returns>BoundaryNodes</returns>
-        public List<Node> boundaryNodes = new List<Node>();
-
-
         public DomainBoundary(int id, double[,] boundaryNodesCoordinates)
         {
             this.Id = id;
             this.BoundaryNodesCoordinates = boundaryNodesCoordinates;
-            CreateNodes(); 
-        }
-
-        private void CreateNodes()
-        {
-            for (int i = 0; i < BoundaryNodesCoordinates.GetLength(0); i++)
-            {
-                var newNode = new Node();
-                var NaturalX = new CoordinateType("NaturalX");
-                var NaturalY = new CoordinateType("NaturalY");
-                newNode.Coordinates.Add(NaturalX, BoundaryNodesCoordinates[i, 0]);
-                newNode.Coordinates.Add(NaturalY, BoundaryNodesCoordinates[i, 1]); //    NatCoord.X = BoundaryNodesCoordinates[i,0];
-                newNode.Id.Boundary = Id;
-                boundaryNodes.Add(newNode);
-            }
         }
     }
 }
