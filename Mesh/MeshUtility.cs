1fef7e4ba13a76e2c8cd289b125db9c476f4d9a1
Priza Toixou <orestis.pps@gmail.com>
2022-09-20 22:52:04 +0300

    now we talking

--- Mesh/MeshUtility.cs
+++ Mesh/MeshUtility.cs
@@ -7,20 +7,20 @@
     {
         static MeshUtility() {}
 
-        public static void CheckInputData(DomainBoundary[] DomainBoundaries)
-        {
-            if (DomainBoundaries.Count() != 4)
-                {throw new Exception("INSERT 4 BOUNDARIES"); }
-            else if (DomainBoundaries[0].Id != 0 || DomainBoundaries[1].Id != 1 || DomainBoundaries[2].Id != 2 || DomainBoundaries[3].Id != 3)
-                {throw new Exception("INSERT BOUNDARIES WITH THIS ORDER : BOTTOM(0) -> RIGHT(1) -> TOP(2) -> LEFT(3)"); }
-            else if  (DomainBoundaries[0].BoundaryNodesCoordinates.GetLength(0) != DomainBoundaries[2].BoundaryNodesCoordinates.GetLength(0)) 
-                {throw new Exception("MISMATCH IN NODE NUMBER IN BOUNDARIES: 0 AND 2");}
-            else if  (DomainBoundaries[1].BoundaryNodesCoordinates.GetLength(0) != DomainBoundaries[3].BoundaryNodesCoordinates.GetLength(0))
-                {throw new Exception("MISMATCH IN NODE NUMBER IN BOUNDARIES: 1 AND 3");}
-            else 
-                {Console.WriteLine("INPUT DATA CHECKED! PROCCEED TO MESH GENERATOR...");}
+        // public static void CheckInputData(DomainBoundary[] DomainBoundaries)
+        // {
+        //     if (DomainBoundaries.Count() != 4)
+        //         {throw new Exception("INSERT 4 BOUNDARIES"); }
+        //     else if (DomainBoundaries[0].Id != 0 || DomainBoundaries[1].Id != 1 || DomainBoundaries[2].Id != 2 || DomainBoundaries[3].Id != 3)
+        //         {throw new Exception("INSERT BOUNDARIES WITH THIS ORDER : BOTTOM(0) -> RIGHT(1) -> TOP(2) -> LEFT(3)"); }
+        //     else if  (DomainBoundaries[0].BoundaryNodesCoordinates.GetLength(0) != DomainBoundaries[2].BoundaryNodesCoordinates.GetLength(0)) 
+        //         {throw new Exception("MISMATCH IN NODE NUMBER IN BOUNDARIES: 0 AND 2");}
+        //     else if  (DomainBoundaries[1].BoundaryNodesCoordinates.GetLength(0) != DomainBoundaries[3].BoundaryNodesCoordinates.GetLength(0))
+        //         {throw new Exception("MISMATCH IN NODE NUMBER IN BOUNDARIES: 1 AND 3");}
+        //     else 
+        //         {Console.WriteLine("INPUT DATA CHECKED! PROCCEED TO MESH GENERATOR...");}
 
-        }
+        // }
 
         public static bool NodeExists(int nodeId, Dictionary<int, Node> nodes)
         {
@@ -34,41 +34,27 @@
             }
         }
         
-        public static void GnuplotBoundary(DomainBoundary[] domainBoundaries)
-        {
-            GnuPlot.HoldOn();
-            GnuPlot.Set("xlabel 'X'") ;
-            GnuPlot.Set("ylabel 'Y'");
-            GnuPlot.Set("grid");
-            foreach (var boundary in domainBoundaries)
-            {
-                var nn = boundary.BoundaryNodesCoordinates.GetLength(0);
-                var x = new double[nn];
-                var y = new double[nn];
-                for (int i = 0; i < nn; i++)
-                {
-                    x[i] = boundary.BoundaryNodesCoordinates[i, 0];
-                    y[i] = boundary.BoundaryNodesCoordinates[i, 1];
-                }
-                GnuPlot.Plot(x, y);
-            }
-            GnuPlot.HoldOff(); 
-        }
-
-        // public static void MeshPrinter(Node[,] nodeMesh)
+        // public static void GnuplotBoundary(DomainBoundary[] domainBoundaries)
         // {
-        //     var nnx = nodeMesh.GetLength(0);
-        //     var nny = nodeMesh.GetLength(1);
-        //     for (int j =0; j < nny; j++)
+        //     GnuPlot.HoldOn();
+        //     GnuPlot.Set("xlabel 'X'") ;
+        //     GnuPlot.Set("ylabel 'Y'");
+        //     GnuPlot.Set("grid");
+        //     foreach (var boundary in domainBoundaries)
         //     {
-        //         for (int i = 0; i < nnx; i++)
+        //         var nn = boundary.BoundaryNodesCoordinates.GetLength(0);
+        //         var x = new double[nn];
+        //         var y = new double[nn];
+        //         for (int i = 0; i < nn; i++)
         //         {
-        //             Console.WriteLine("ID " + nodeMesh[i, j].Id.Global + " B ID: " 
-        //             + nodeMesh[i,j].Id.Boundary +  " I ID: " + nodeMesh[i,j].Id.Internal 
-        //             + " x: " + nodeMesh[i,j].Coordinates[NaturalX].value + " y: " + nodeMesh[i,j].NatCoord.Y);
+        //             x[i] = boundary.BoundaryNodesCoordinates[i, 0];
+        //             y[i] = boundary.BoundaryNodesCoordinates[i, 1];
         //         }
+        //         GnuPlot.Plot(x, y);
         //     }
+        //     GnuPlot.HoldOff(); 
         // }
+
         public static void GnuplotMesh  (double[,] x, double[,] y)
         {
             var nnx = x.GetLength(0);
